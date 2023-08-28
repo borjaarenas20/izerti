6a030b49e5e2d8b3d3fc03528b21e1fa93f8cc12
Borja Arenas <borjaarenas20@gmail.com>
2023-08-28 16:07:15 +0200

    create testdata and refactor

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,23 +1,24 @@
-let url = "https://www.oysho.com/es/logon/"
-let title = 'Iniciar Sesión | OYSHO España / Spain'
-let user = "borjaarenas@yopmail.com";
-let pass = 123456;
-
-
 describe('test logon url', function () {
   beforeEach(function () {
-    cy.visit(url);
+    cy.fixture('testdata').then(testdata =>{
+      this.testdata = testdata
+    })
+    
+  })
+
+  it('visit url', function(){
+    cy.visit(this.testdata.url);
     cy.get('#onetrust-accept-btn-handler').click()
   })
 
   it('verificate title', function(){
-    cy.title().should('eq',title);
+    cy.title().should('eq',this.testdata.title);
   })
 
 
   it('login', function(){
-    cy.get('.login__form > [data-testid="input-email"] > [data-testid="input-style"]').type(user);
-    cy.get('[data-testid="input-password"] > [data-testid="input-style"]').type(pass);
+    cy.get('.login__form > [data-testid="input-email"] > [data-testid="input-style"]').type(this.testdata.user);
+    cy.get('[data-testid="input-password"] > [data-testid="input-style"]').type(this.testdata.pass);
     cy.get('[data-testid="login-button"]').click();
     
   })
